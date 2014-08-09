b0263b6e41214d24a29c1ceeeac36bb76f243a5a
muge <xiaoxiaomuge@163.com>
2014-08-09 17:13:01 +0800

    吃什么项目基础文件调整

--- ajaxpage/ajax.aspx.cs
+++ ajaxpage/ajax.aspx.cs
@@ -5,22 +5,36 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using Newtonsoft.Json;
+
 public partial class ajaxpage_ajax : System.Web.UI.Page
 {
+    JavaScriptObject resultObj = new JavaScriptObject ( );
     protected void Page_Load(object sender, EventArgs e)
     {
         string option = Request["option"];
-
         switch (option)
         {
-            case "a":
+            case "GetPointer":
+                {
+                    GetPointer ( );
+                    break;
+                }
+            default:
                 {
                     break;
                 }
-            default
-                {
-            break;
-            }
         }
+
+        Response.Clear ( );
+        Response.AddHeader ( "charset", "UTF-8" );
+        Response.AddHeader ( "Content-Type", "Application/json" );
+        Response.Write ( JavaScriptConvert.SerializeObject ( resultObj ) );
+        Response.End ( );
+    }
+
+    public void GetPointer ( ) 
+    {
+        resultObj.Add ( "result", Common.GetPointer ( ) );
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
 ﻿
-var index = new Object();
+var Index = new Object();
 
-index.option = {
-a:"a"
+Index.Init = function () {
+    Index.BindEvent();
 }
 
-index.get = function () {
+Index.BindEvent= function () {
+    $(".btn_userAuto").click(function () {
+        $.layer({
+            type: 1,
+            shade: [0],
+            area: ['auto', 'auto'],
+            title: false,
+            border: [0],
+            page: {
 
+                html: '<div style="width:420px; height:260px; padding:20px; border:1px solid #06c; background-color:#eee;">demo</div>'
+            }
+        });
+
+    });
 }
+
+$(function () {
+    Index.Init();
+});
