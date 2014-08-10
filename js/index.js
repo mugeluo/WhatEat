bd0d8b58de63e1a3872f57112ce3794b5dc6b935
muge <xiaoxiaomuge@163.com>
2014-08-10 09:54:19 +0800

    界面样式调整

--- js/index.js
+++ js/index.js
@@ -1,43 +1,70 @@
-﻿
-var Index = new Object();
+﻿var Index = new Object();
 Index.Option =
 {
-    layerIndex:0
+    layerIndex:0,
+    screenWidth:0,
+    userChangeCount:0
 }
 
 Index.Init = function () {
+    var screenWidth = document.body.clientWidth;
+    Index.Option.screenWidth = screenWidth - 80;
+
     Index.BindEvent();
 }
 
-Index.BindEvent= function () {
-    $(".btn_userAuto").bind("click",function () {
-        var html = '<div style="width:420px; height:260px; padding:20px;border:2px solid #c07452; background-color:#eee;border-radius:4px;">';
-        html += '<input type="button" onclick="Index.DialogClose();" value="close"/>';
-        html += '</div>';
+Index.BindEvent = function () {
+    $("#btn_userAuto").bind("click", function () {
+        Index.ShowPopup(Index.CreateHtmlWithDialog());
+    });
 
-        Index.Option.layerIndex = $.layer({
-            type: 1,
-            shade: [0.3, '#000'],
-            area: ['auto', 'auto'],
-            title: false,
-            closeBtn: [0, true],
-            border: [0],
-            shift: "top",
-            fix: true,
-            page: {
-                html: html
-            }
-        });
-
+    $("#btn_userListenGog").bind("click", function () {
+        Index.ShowPopup(Index.CreateHtmlWithDialog2());
     });
 }
 
-Index.DialogClose = function () {
-    layer.close(Index.Option.layerIndex);
+Index.ChangeNextShop = function () {
+    Index.ShowPopup( Index.CreateHtmlWithDialog() );
+}
 
-    var html = '<div style="width:420px; height:260px; padding:20px;border:2px solid #c07452; background-color:#eee;border-radius:4px;">';
-    html += '<input type="button" onclick="Index.DialogClose();" value="close"/>';
+Index.CreateHtmlWithDialog = function () {
+    Index.Option.userChangeCount = Index.Option.userChangeCount + 1;
+    if (Index.Option.userChangeCount < 4) {
+        $("#userChangeCount").html(Index.Option.userChangeCount);
+
+        var html = '<div style="width:' + Index.Option.screenWidth + 'px; height:260px; padding:20px;border:2px solid #c07452; background-color:#eee;border-radius:4px;">';
+        html += '<div><img src="images/ico_gotoit.png" /></div>';
+        html += '<div style="backgroup-color:#fff;padding:10px;">';
+        html += '<div class="pAll10">兰州拉面</div>';
+        html += '<div class="TxtCenter"><img class="dialogBtnSize" src="images/ico_gotoit.png"/></div>';
+        html += '<div class="mTop10 TxtCenter"><img class="dialogBtnSize" onclick="Index.ChangeNextShop();" src="images/ico_changeNext.png"/></div>';
+        html += '</div>';
+        html += '</div>';
+    }
+    else
+     {
+         var html = '<div style="width:' + Index.Option.screenWidth + 'px; height:260px; padding:20px;border:2px solid #c07452; background-color:#eee;border-radius:4px;">';
+         html += '<div class="pAll10">兰州拉面</div>';
+         html += '<div class="mTop10 TxtCenter"><img class="dialogBtnSize" src="images/btn_godKnow.png"/></div>';
+         html += '</div>';
+     }
+
+    return html;
+}
+Index.CreateHtmlWithDialog2 = function () {
+    var html = '<div style="width:' + Index.Option.screenWidth + 'px; height:260px; padding:20px;border:2px solid #c07452; background-color:#eee;border-radius:4px;">';
+    html += '<div><img src="images/ico_gotoit.png" /></div>';
+    html += '<div style="backgroup-color:#fff;padding:10px;">';
+    html += '<div class="pAll10">兰州拉面</div>';
+    html += '<div class="mTop10 TxtCenter"><img class="dialogBtnSize" src="images/ico_gotoit.png"/></div>';
     html += '</div>';
+    html += '</div>';
+
+    return html;
+}
+
+Index.ShowPopup = function (html) {
+    layer.close(Index.Option.layerIndex);
 
     Index.Option.layerIndex = $.layer({
         type: 1,
@@ -59,7 +86,7 @@ $(function () {
 
     setTimeout(function () {
         $(".pageLayer").hide();
-        $("body").css("backgroundColor", "#fff");
+        $("#page").css("background", "none");
         $(".pageAll").show();
     }, 1000);
 
